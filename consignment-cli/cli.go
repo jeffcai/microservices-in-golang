ea5368f790e650da54fb93cf21b280bd2b77aab9
Jeff Cai <jeff.caijf@gmail.com>
2018-03-18 10:12:31 +0800

    imported the go-micro to reimplement consignment services
    - regenerated protobuf
    - updated consignment service and its cli
    - updated make file

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -1,5 +1,3 @@
-// consignment-cli/cli.go
-
 package main
 
 import (
@@ -9,12 +7,12 @@ import (
 	"os"
 
 	pb "github.com/microservices-in-golang/consignment-service/proto/consignment"
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
 )
 
 const (
-	address         = "localhost:50051"
 	defaultFilename = "consignment.json"
 )
 
@@ -29,13 +27,11 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+
+	cmd.Init()
+
+	// Create new greeter client
+	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
 
 	// Contact the server and print out its response.
 	file := defaultFilename
@@ -49,9 +45,9 @@ func main() {
 		log.Fatalf("Could not parse file: %v", err)
 	}
 
-	r, err := client.CreateConsignment(context.Background(), consignment)
+	r, err := client.CreateConsignment(context.TODO(), consignment)
 	if err != nil {
-		log.Fatalf("Could not greet: %v", err)
+		log.Fatalf("Could not create: %v", err)
 	}
 	log.Printf("Created: %t", r.Created)
 
